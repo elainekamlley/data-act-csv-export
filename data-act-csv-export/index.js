24984400afca08667eac3b12a80127b4409f7199
Marco Secreto <msegreto@miceover.com>
2015-05-14 10:44:27 -0700

    Basic json2csv setup

--- data-act-csv-export/index.js
+++ data-act-csv-export/index.js
@@ -1,23 +1,25 @@
 var fs = require('fs');
 var json2csv = require('json2csv');
-var linter = require('./linter');
 
-var file = fs.readFile(jsonFilename)
-var converted = Converter.convert(file);
-var final = linter(converted);
+var fakeData = {
+  'issue number': 32,
+  'issue title': 'Something is wrong',
+  'comments': [
+    { },
+    { }
+  ]
+};
 
-json2csv {data: someJSONData, fields: ['field1', 'field2', 'field3']} {
-  fs.readFile( jsonFilename, csv, function(err, data){
-    if (err) {
-    console.warning(err);
+
+json2csv({ data: fakeData, fields: ['issue number', 'issue title'] },
+    function(err, data) {
+
+  if (err) {
+    console.error(err);
     process.exit(1);
-    };
-  };
+  }
+  console.log(data);
+  process.exit(0);
 
-  var s = json2csv({
-    data: data,
-    fields: ['issue number', 'issue title', 'comments']
-  });
-  console.log(s);
-  fs.writeFile('data.csv', s, function(err)
 });
+
