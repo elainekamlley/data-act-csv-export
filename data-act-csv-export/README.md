8ec80c838e121b9deafe082623fd0e0c4784e8ca
Elaine Kamlley <elaine.kamlley@gsa.gov>
2015-05-27 15:29:26 -0400

    cleaned up the files and added README

---
+++ data-act-csv-export/README.md
@@ -0,0 +1,7 @@
+This is a node script that extracts GitHub issues and comments then exports the data into a csv. 
+
+### Package Dependencies 
+
+- [octonode](https://github.com/pksunkara/octonode) as a GitHub API wrapper
+- [json2csv](https://www.npmjs.com/package/json2csv) package to convert the GitHub JSON data and export as a csv
+- [async](https://www.npmjs.com/package/async)
--- data-act-csv-export/index.js
+++ data-act-csv-export/index.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var fs = require('fs');
 var github =require('octonode');
 
@@ -8,58 +9,85 @@ var client = github.client(process.env.GITHUB_API_KEY);
 var ghrepo = client.repo('fedspendingtransparency/fedspendingtransparency.github.io');
 
 ghrepo.issues({
-  state: 'all',
+ 	state: 'all',
+ 	per_page: 100,
 }, function(err, body, status, headers) {
-  var issues = body;
-  for (var i = 0, ilen = issues.length; i < ilen; i++) {
-    var issue = issues[i],
-      issueData = {
-        "Issue Number": issue.number,
-        "Issue Title": issue.title, 
-        "Issue Description": issue.body,
-        "Data Element": issue.lables,
-        "Date Opened": issue.created_at,
-        "Date Closed": issue.closed_at,
-      };
-    json2csv({ data: issueData, fields: ['Issue Number', 'Issue Title', 'Issue Description', 'Data Element', 'Date Opened', 'Date Closed'] },
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	var issues = body,
+      issuesData = [],
+      commentsReqs = [];
+
+	for (var i = 0, ilen = issues.length; i < ilen; i++) {
+		var issue = issues[i],
+			issueData = {
+				"Issue Number": issue.number,
+				"Issue Title": issue.title,
+				"Issue Description": issue.body,
+				"Date Opened": issue.created_at,
+				"Date Closed": issue.closed_at,
+			};
+		issuesData.push(issueData);
+
+    (function(issueNumber, issueData) {
+      commentsReqs.push(function(cb) {
+        var currentIssue = client.issue(
+            'fedspendingtransparency/fedspendingtransparency.github.io',
+            issueNumber);
+        currentIssue.comments(function(err, body, headers) {
+          var comments = body,
+              commentData = '';
+          console.log('current', currentIssue.number);
+          console.log('comment error', err);
+          if (!comments) {
+            cb(err, comments);
+          }
+          for (var c = 0, clen = comments.length; c < clen; c++) {
+            var comment = comments[c],
+                commentData = {
+                  "Issue url": comment.issue_url,
+                  "Author": comment.user.login,
+                  "Comment": comment.body
+                };
+            issueData['Comment'+ c] = comment.body;
+            issueData['CommentAuthor'+ c] = commentData.Author;
+          };
+          cb(err, commentData);
+        });
+      });
+    })(issue.number, issueData);
+	}
+  async.series(commentsReqs, function(err, results) {
+  	var fields = ['Issue Number', 'Issue Title', 'Issue Description',
+               'Date Opened', 'Date Closed'];
+   	for (var c = 1, clen = 30; c < clen; c++){
+   		fields.push('CommentAuthor' + c);
+   		fields.push('Comment' + c);
+   	}
+    console.log('error', err);
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    json2csv({ data: issuesData,
+               fields: fields },
       function(err, data) {
         if (err) {
-              console.error(err);
-              process.exit(1);
-            }
-            console.log('in json2csv');
-            fs.writeFile('./data.csv', data, function (err) {
-              if (err) {
-                console.error(err);
-                process.exit(1);
-              }
-              console.log('It\'s saved!');
-    console.log(issueData);
-    var asyncFuncs = [];
-    for (var i = 0, ilen = list.lenghth; i < ilen; i++) {
-      var ghissue = client.issue('fedspendingtransparency/fedspendingtransparency.github.io', issue[i].number); 
-      asyncFuncs.push(ghissue.comments);
-    }
-    when(asyncFuncs).then(function(err,body,headers) {
-      json2csv({ data: issueData, fields: ['Issue Number', 'Issue Title', 'Issue Description', 'Data Element', 'Date Opened', 'Date Closed'] },
-      function(err, data) {
-        if (err) {
-              console.error(err);
-              process.exit(1);
-            }
-            console.log('in json2csv');
-            fs.writeFile('./data.csv', data, function (err) {
-              if (err) {
-                console.error(err);
-                process.exit(1);
-              }
-              console.log('It\'s saved!');
-            });
-      }
-    }
+          console.error(err);
+          process.exit(1);
+        }
+        var fileName = './GitHub-export' + (new Date()).toJSON() + '.csv';
+        fs.writeFileSync(fileName, "");
+        fs.appendFile(fileName, data, function (err) {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+          console.log('It\'s saved!');
+        });
+    });
   });
 });
- 
-  
-
 
